0b3133eb3f83dbe272b9640c78bf13446a913626
HarutoYamauchi <haruto1391@icloud.com>
2025-07-14 00:52:51 +0900

    時間経過でEnemyが消える機能の追加

--- Assets/Script/EnemyMove.cs
+++ Assets/Script/EnemyMove.cs
@@ -3,11 +3,16 @@ using UnityEngine;
 public class EnemyMoveb : MonoBehaviour
 {
     [SerializeField] private float enemySpeed = -5;
-
+    [SerializeField] private float enemyLifeTime;//追加
     void Update()
     {
         transform.Translate(new Vector3(0, 0, enemySpeed) * Time.deltaTime);//敵が下方向に落ちてくる
 
-
+        //以下のコードを追加
+        //もしLifeTimeが０より小さくなったら
+        if (enemyLifeTime < 0)
+        {
+            Destroy(this.gameObject);//このオブジェクトを削除
+        }
     }
 }
